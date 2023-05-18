11b23ee6e2e2a648cd8ca272ca6ccf6620ddbda0
João Vítor Pinto Vizeu <jvpvizeu@MacBook-Air-de-Joao.local>
2023-05-18 15:54:17 -0300

    0.01v

---
+++ src/definitions.c
@@ -0,0 +1,4 @@
+#include <stdio.h>
+#include <stdlib.h>
+int players = 4; //criar uma função aqui depois que o menu principal for criado
+int position[4]; //40 casas na tabuleiro, o segundo eixo determina o player(achar forma de criar vetor de tamanho variável)
---
+++ src/game.c
@@ -0,0 +1,51 @@
+#include "definitions.c"
+
+int clearScreen(){
+    system("clear");
+}
+int printTable(){
+    clearScreen();
+    printf("\n");
+    printf("\n                ST.CH  ELCT  STATES VRGNIA PNSLVA  ST.JA   COM    TEN    NY    FREE         ");
+    printf("\n                 AVE   COMP    AVE    AVE   RAIL    AVE   CHEST   AVE   AVE    PARK         ");
+    printf("\n        ____________________________________________________________________________        ");
+    printf("\n VISIT |      |      |      |      |      |      |      |      |      |      |      |       ");
+    printf("\n  JAIL |      |      |      |      |      |      |      |      |      |      |      |       ");
+    printf("\n       |______|______|______|______|______|______|______|______|______|______|______|       ");
+    printf("\n CNNCT |      |                                                              |      | KNTCY ");
+    printf("\n  AVE  |      |                                                              |      |  AVE  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n VERMNT|      |                                                              |      |       ");
+    printf("\n  AVE  |      |                                                              |      |CHANCE ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n       |      |                                                              |      |  IND  ");
+    printf("\n CHANCE|      |                                                              |      |  AVE  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n ORNTL |      |                                                              |      |  ILL  ");
+    printf("\n  AVE  |      |                                                              |      |  AVE  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n  READ |      |                                                              |      | B&O.  ");
+    printf("\n  RAIL |      |                                                              |      | RAIL  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n       |      |                                                              |      |ATLNTC ");
+    printf("\n  TAX  |      |                                                              |      |  AVE  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n BALTIC|      |                                                              |      | VNTNR ");
+    printf("\n  AVE  |      |                                                              |      |  AVE  ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n  COM  |      |                                                              |      | WATER ");
+    printf("\n CHEST |      |                                                              |      | WORKS ");
+    printf("\n       |______|                                                              |______|       ");
+    printf("\n MEDTRN|      |                                                              |      | MRVN  ");
+    printf("\n  AVE  |      |                                                              |      | GRDNS ");
+    printf("\n       |______|______________________________________________________________|______|       ");
+    printf("\n       |      |      |      |      |      |      |      |      |      |      |      | GO TO ");
+    printf("\n START |      |      |      |      |      |      |      |      |      |      |      |  JAIL ");
+    printf("\n       |______|______|______|______|______|______|______|______|______|______|______|       ");
+    printf("\n                BRDWK LUXURY  PARK  CHANCE  SHORT PNSLVA   COM    NC    PCFC                ");
+    printf("\n                       TAX    PLACE         LINE    AVE   CHEST   AVE   AVE                 ");
+}
+int main(){
+printTable();
+return 0;
+}
